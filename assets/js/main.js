4a861d156befcfd6de58b52b3091c2ee9fc10611
jonhsilvaprojetos <jonhsilvaprojetos@gmail.com>
2021-02-05 00:16:34 -0300

    Ajustes finais home

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,17 @@
 /*/ CARROUSEL /*/
 
 $("#banners").slick({
-    dots: true,
+    draggable: true,
+    autoplay: true,
     autoplaySpeed: 5000,
-    infinite: true,
-    pauseOnHover:false,
-    autoplay:true,
     arrows: false,
-    speed: 1000,
-    slidesToShow: 1,
-    centerMode: true,
-    variableWidth: true,
-    easing:'linear',
+    dots: true,
+    fade: true,
+    speed: 800,
+    infinite: true,
+    cssEase: 'ease-in-out',
+    touchThreshold: 100,
+    adaptiveHeight: true,
+    pauseOnHover: false,
+    pauseOnFocus: false,
 });
