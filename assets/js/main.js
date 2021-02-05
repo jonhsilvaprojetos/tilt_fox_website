e0d86694cb678035a2b3af80596138b17246951b
jonhsilvaprojetos <jonhsilvaprojetos@gmail.com>
2021-02-04 23:40:20 -0300

    primeira pagina

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+/*/ CARROUSEL /*/
+
+$("#banners").slick({
+    dots: true,
+    autoplaySpeed: 5000,
+    infinite: true,
+    pauseOnHover:false,
+    autoplay:true,
+    arrows: false,
+    speed: 1000,
+    slidesToShow: 1,
+    centerMode: true,
+    variableWidth: true,
+    easing:'linear',
+});
